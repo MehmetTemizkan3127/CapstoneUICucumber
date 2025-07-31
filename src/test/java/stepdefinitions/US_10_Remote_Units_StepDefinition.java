dc7ca01829f9b130e6fad53f5d6b324338ad9b73
Ferhat <losandess258@gmail.com>
2025-07-31 12:59:19 -0400

    testcase10

---
+++ src/test/java/stepdefinitions/US_10_Remote_Units_StepDefinition.java
@@ -0,0 +1,61 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import pages.HomePage;
+import pages.LoginPage;
+import pages.RemoteUnitsPage;
+import pages.UsersPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.JavascriptUtils;
+import utilities.ReusableMethods;
+
+import java.util.List;
+
+public class US_10_Remote_Units_StepDefinition {
+    RemoteUnitsPage remoteUnitsPage = new RemoteUnitsPage();
+    HomePage homePage=new HomePage();
+    LoginPage loginPage=new LoginPage();
+   JavascriptUtils javascriptUtils=new JavascriptUtils();
+
+    @When("user clicks on Login button")
+    public void user_clicks_on_login_button() {
+        homePage.login();
+
+    }
+    @When("the user enters a valid {string} username in the username field")
+    public void the_user_enters_a_valid_username_in_the_username_field(String string) {
+        loginPage.enterEmail(ConfigReader.getProperty("usuario"));
+
+    }
+    @When("the user enters a valid {string} password in the Password field")
+    public void the_user_enters_a_valid_password_in_the_password_field(String string) {
+        loginPage.enterPassword(ConfigReader.getProperty("clave"));
+    }
+    @When("the user clicks on the Sign in button")
+    public void the_user_clicks_on_the_sign_in_button() {
+loginPage.clickSignIn();
+        try {Thread.sleep(2000);} catch (InterruptedException e) {}
+    }
+    @When("user clicks the Remote Units  menu")
+    public void user_clicks_the_remote_units_menu() {
+        remoteUnitsPage.dropdown.click();
+remoteUnitsPage.remoteunitsign.click();
+
+
+    }
+
+    @And("user verify the Remote units page menu")
+    public void userVerifyTheRemoteUnitsPageMenu() {
+       ReusableMethods.waitForSeconds(3);
+        int expectedSize = 35;
+        int actualSize=remoteUnitsPage.displayRemotePagelist.size();
+        System.out.println("actualSize = " + actualSize);
+        Assert.assertEquals(actualSize,expectedSize);
+    }
+}
--- src/test/resources/features/RemoteUnit.feature
+++ src/test/resources/features/RemoteUnit.feature
@@ -1,1 +1,11 @@
-Feature:
+@US_0010
+Feature:displayed remote units
+  @Display_Created_Remote_Unit
+  Scenario: The user displays the created remote unit
+    Given user goes to homepage
+    When user clicks on Login button
+    And the user enters a valid "usuario" username in the username field
+    And the user enters a valid "clave" password in the Password field
+    And the user clicks on the Sign in button
+    And user clicks the Remote Units  menu
+    And user verify the Remote units page menu
