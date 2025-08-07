4cba7900c4d414707f3a483aa580e764b8f3f1e3
Ferhat <losandess258@gmail.com>
2025-08-07 11:36:31 -0400

    son2

--- src/test/java/stepdefinitions/US_010_RemoteUnitsSD.java
+++ src/test/java/stepdefinitions/US_010_RemoteUnitsSD.java
@@ -53,7 +53,7 @@ loginPage.clickSignIn();
     public void userVerifyTheRemoteUnitsPageMenu() throws InterruptedException {
         JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
         waitForSeconds(3);
-        int expectedSize = 64;
+        int expectedSize = 63;
         int actualSize=remoteUnitsPage.displayRemotePagelist.size();
         System.out.println("actualSize = " + actualSize);
         Assert.assertEquals(actualSize,expectedSize);
